b9da5563a50eb7ea0ca3446781e4cdbabf9faba4
Behtarin Ferdousi <behtarin.ferdousi@autoscout24.com>
2024-01-04 17:57:15 +0100

    log info

--- logging-in-a-spring-boot-project-start/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
+++ logging-in-a-spring-boot-project-start/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
@@ -2,6 +2,9 @@ package com.baeldung.ls.service.impl;
 
 import java.util.Optional;
 
+import com.baeldung.ls.persistence.repository.impl.ProjectRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.baeldung.ls.persistence.model.Project;
@@ -11,6 +14,7 @@ import com.baeldung.ls.service.IProjectService;
 @Service
 public class ProjectServiceImpl implements IProjectService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectRepositoryImpl.class);
     private IProjectRepository projectRepository;
 
     public ProjectServiceImpl(IProjectRepository projectRepository) {
@@ -19,11 +23,13 @@ public class ProjectServiceImpl implements IProjectService {
 
     @Override
     public Optional<Project> findById(Long id) {
+        LOG.info("Project service >> Finding project by ID {} ", id);
         return projectRepository.findById(id);
     }
 
     @Override
     public Project save(Project project) {
+        LOG.info("Project Service >> Saving Project");
         return projectRepository.save(project);
     }
 
